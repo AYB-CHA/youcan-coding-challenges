b4bc8ca7ea9a9e64ea3f94af6439cd144186a0dd
Ayoub Chaaoui <ayoubchaaoui02@gmail.com>
2024-01-09 13:00:26 +0100

    feat(frontend/useFecth): add query params support to help with filtering

--- frontend/src/composables/useFetch.ts
+++ frontend/src/composables/useFetch.ts
@@ -3,22 +3,26 @@ import axios from '@/lib/axios'
 import type { AxiosError } from 'axios';
 import { ref, type Ref } from 'vue'
 
+type ParamsType = Record<string, string | undefined>;
+
 interface ResultType<T> {
     data: Ref<T | null>;
     isLoading: Ref<boolean>;
     error: Ref<AxiosError | null>;
-    mutate: () => Promise<void>;
+    mutate: (params: ParamsType) => Promise<void>;
 }
 
-export function useFetch<T = any>(endpoint: string): ResultType<T> {
+export function useFetch<T = any>(endpoint: string, params: ParamsType = {}): ResultType<T> {
     const data = ref<any>(null);
     const isLoading = ref(false);
     const error = ref<AxiosError | null>(null);
 
-    const mutate = async () => {
+    const mutate = async (params: ParamsType = {}) => {
         try {
             isLoading.value = true;
-            const response = await axios.get(endpoint);
+            const response = await axios.get(endpoint, {
+                params,
+            });
             data.value = response.data;
         } catch (e) {
             error.value = (e as AxiosError);
@@ -29,7 +33,7 @@ export function useFetch<T = any>(endpoint: string): ResultType<T> {
         }
     }
 
-    mutate();
+    mutate(params);
 
     return { data, isLoading, error, mutate };
 }
